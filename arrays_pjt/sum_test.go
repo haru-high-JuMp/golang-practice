ad859f97d952fdf5a97f512fbeff7e6f739a2af7
Haruki Mori <haru0dev@gmail.com>
2022-08-21 11:34:58 +0900

    create: SumAllTails function in arrays_pjt

--- arrays_pjt/sum_test.go
+++ arrays_pjt/sum_test.go
@@ -39,3 +39,27 @@ func TestAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v ", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9, 3})
+		want := []int{2, 12}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+}
